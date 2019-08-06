9882fd97121a67de3df51c0c7538553a28f6814a
Andrew Henning-Kolberg <andrew.kolberg@gmail.com>
2019-08-06 23:52:58 +0000

    Add app-3, v-if example

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -1,6 +1,7 @@
 import Vue from 'vue/dist/vue.esm'
 
 document.addEventListener('DOMContentLoaded', () => {
+  // first app
   var app = new Vue({
     el: '#app',
     data: {
@@ -8,10 +9,19 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   })
 
+  // v-bind
   var app2 = new Vue({
     el: '#app-2',
     data: {
       message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+  })
+
+  // v-if
+  var app3 = new Vue({
+    el: '#app-3',
+    data: {
+      seen: true
     }
   })
 })
