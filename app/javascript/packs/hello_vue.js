9bf21476660742b54fca896b699c17e65c985dd3
Andrew Henning-Kolberg <andrew.kolberg@gmail.com>
2019-08-07 00:05:05 +0000

    Add app-4, v-for

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -24,4 +24,16 @@ document.addEventListener('DOMContentLoaded', () => {
       seen: true
     }
   })
+
+  // v-for
+  var app4 = new Vue({
+    el: '#app-4',
+    data: {
+      todos: [
+        { text: 'Learn JavaScript' },
+        { text: 'Learn Vue' },
+        { text: 'Build something awesome' },
+      ]
+    }
+  })
 })
