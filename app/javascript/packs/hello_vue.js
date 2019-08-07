8a408b11a8706bc8cbb1f72d44ae284b2b4bc119
Andrew Henning-Kolberg <andrew.kolberg@gmail.com>
2019-08-07 00:11:50 +0000

    Add app-5, v-on

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -36,4 +36,17 @@ document.addEventListener('DOMContentLoaded', () => {
       ]
     }
   })
+
+  // v-on
+  var app5 = new Vue({
+    el: '#app-5',
+    data: {
+      message: 'Hello Vue.js'
+    },
+    methods: {
+      reverseMessage: function () {
+        this.message = this.message.split(' ').reverse().join(' ')
+      }
+    }
+  })
 })
